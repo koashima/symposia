1cb0e62e8d7f8019ec5629d4fb301a4c917c6d0e
koashima <48111257+koashima@users.noreply.github.com>
2020-04-13 15:47:44 -0700

    added passport to server.js, exported database module

--- symposia/server.js
+++ symposia/server.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session');
+const passport = require('passport');
 
 require('dotenv').config();
 
@@ -10,6 +12,9 @@ var indexRouter = require('./routes/index');
 var symposiaRouter = require('./routes/symposia');
 
 var app = express();
+
+require('./config/database');
+require('./config/passport');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,6 +25,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({
+    secret: 'SWAG',
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/symposia', symposiaRouter);
