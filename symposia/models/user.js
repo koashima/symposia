235a04ad9a0202b3c47a714fe50a948556fe761a
koashima <48111257+koashima@users.noreply.github.com>
2020-04-13 16:10:45 -0700

    added googleId property to userSchema

--- symposia/models/user.js
+++ symposia/models/user.js
@@ -1,1 +1,15 @@
 const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String },
+    email: { type: String },
+    avatar: { type: String },
+    googleId: { type: String },
+  },
+  {
+    timpestamps: true,
+  }
+);
+
+module.exports = mongoose.model('User', userSchema);
