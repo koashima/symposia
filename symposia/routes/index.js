3e6aba0f42bd0cb1c6e121278b8e76bf7f6e1c4b
koashima <48111257+koashima@users.noreply.github.com>
2020-04-13 22:27:14 -0700

    added routes for google OAuth

--- symposia/routes/index.js
+++ symposia/routes/index.js
@@ -1,7 +1,24 @@
 var express = require('express');
 var router = express.Router();
+const passport = require('passport');
 
 /* GET home page. */
 router.get('/', (req, res, next) => res.redirect('/symposia'));
+
+router.get(
+  '/auth/google', passport.authenticate(
+    'google',
+    { scope: ['profile', 'email'] })
+);
+
+router.get('oauth2callback', passport.authenticate(
+    'google', 
+    {
+    successRedirect: '/symposia',
+    failureRedirect: '/symposia'     
+    })
+);
+   
+
 
 module.exports = router;
