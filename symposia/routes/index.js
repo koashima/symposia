ee3369b82bba0a2f74113742bdf72b0d45c97f0a
koashima <48111257+koashima@users.noreply.github.com>
2020-04-14 16:00:41 -0700

    fixed google OAuth bug

--- symposia/routes/index.js
+++ symposia/routes/index.js
@@ -3,15 +3,14 @@ var router = express.Router();
 const passport = require('passport');
 
 /* GET home page. */
-router.get('/', (req, res, next) => res.redirect('/symposia'));
+router.get('/', (req, res) => res.redirect('/symposia'));
 
-router.get(
-  '/auth/google', passport.authenticate(
+router.get('/auth/google', passport.authenticate(
     'google',
-    { scope: ['profile', 'email'] })
-);
+    { scope: ['profile', 'email'] }
+));
 
-router.get('oauth2callback', passport.authenticate(
+router.get('/oauth2callback', passport.authenticate(
     'google', 
     {
     successRedirect: '/symposia',
--- symposia/routes/symposia.js
+++ symposia/routes/symposia.js
@@ -4,6 +4,6 @@ const symposiaCtrl = require('../controllers/symposia');
 
 router.get('/', symposiaCtrl.index);
 
-router.get('/profile', () => {res.send('this is the profile page')})
+// router.get('/profile', () => {res.send('this is the profile page')})
 
 module.exports = router;
