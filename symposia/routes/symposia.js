6d0b9536b13f069fd23a1a28d5d409da815c6ef7
koashima <48111257+koashima@users.noreply.github.com>
2020-04-16 10:48:23 -0700

    added isAuth middleware

--- symposia/routes/symposia.js
+++ symposia/routes/symposia.js
@@ -2,9 +2,13 @@ var express = require('express');
 var router = express.Router();
 const symposiaCtrl = require('../controllers/symposia');
 
+const isAuth = (req, res, next) => { 
+    req.isAuthenticated() ? next() : res.redirect('/auth/google');    
+};
+
 router.get('/', symposiaCtrl.index);
-router.get('/new', symposiaCtrl.new);
-router.get('/chronicle', symposiaCtrl.chronicle);
+router.get('/new', isAuth, symposiaCtrl.new);
+router.get('/chronicle', isAuth, symposiaCtrl.chronicle);
 router.post('/chronicle', symposiaCtrl.create);
 
 module.exports = router;
