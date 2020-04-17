b1e83fa56ec26b48ac4dc49d5257c01100a06de8
koashima <48111257+koashima@users.noreply.github.com>
2020-04-17 10:01:04 -0700

    redid lost changes

--- symposia/routes/symposia.js
+++ symposia/routes/symposia.js
@@ -11,8 +11,8 @@ router.get('/new', isAuth, symposiaCtrl.new);
 
 router.get('/chronicle', isAuth, symposiaCtrl.chronicle);
 router.post('/chronicle', isAuth, symposiaCtrl.create);
-
 router.get('/list', isAuth, symposiaCtrl.list);
+router.get("/:id/edit", symposiaCtrl.edit);
 router.get('/:id',  isAuth, symposiaCtrl.show);
 router.delete('/:id', symposiaCtrl.delsymposium);
 router.put('/:id', symposiaCtrl.update);
